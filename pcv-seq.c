fffb67ae807df7ad412aa6043cebd7730dec9ac2
Pedro Souza <pedrohdjs@outlook.com>
2022-11-29 11:58:32 -0300

    WIP: Sequential version

--- pcv-seq.c
+++ pcv-seq.c
@@ -6,12 +6,28 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*
-****************** Constantes ******************
+****** Constantes e definições de tipos ********
 */
 
 #define MAX_COST 50 //Peso máximo de uma aresta
+#define MAX_GRAPH_SIZE 32 //Tamanho maximo do grafo
+#define MAX_PATH_SIZE (MAX_GRAPH_SIZE+1) //Tamanho máximo de um caminho
+#define STARTING_NODE 0
+#define COST_NOT_COMPUTED 0
+#define PATH_LIST_SIZE 32
+
+typedef struct _path { //Um caminho
+    int nodes[MAX_PATH_SIZE]; //Os nós no caminho
+    int cost; //Custo do caminho até então
+    int size; //Número de nós no caminho
+} path;
+
+typedef struct _path_list{ //Uma lista de caminhos alocados dinamicamente. Os caminhos nessa lista sempre devem ter o mesmo custo.
+    path** paths;
+} path_list;
 
 /*
 *********** Utilidades para matrizes ***********
@@ -81,6 +97,99 @@ void print_matrix(int** matrix, int n, int m){
 }
 
 /*
+************* Utilidades para paths *************
+*/
+
+/**
+ * Cria um novo path vazio
+ * 
+ * @returns um path alocado dinamicamente
+*/
+path* new_path(){
+    path* p = (path*)malloc(1*sizeof(path));
+    for(int i = 0; i<MAX_PATH_SIZE; i++){
+        p->nodes[i] = -1;
+    }
+    p->cost = COST_NOT_COMPUTED;
+    p->size = 0;
+    return p;
+}
+
+/**
+ * Libera a memória alocada para p
+ * 
+ * @param p o path a ser desalocado
+*/
+void delete_path(path* p){
+    free(p);
+}
+
+/**
+ * Cria uma cópia de um path alocada dinamicamente
+ * 
+ * @param original o path a ser copiado
+ * @param n o número máximo de nós no caminho
+ * 
+ * @returns uma cópia de original
+*/
+path* copy_path(path* original){
+    path* p = new_path();
+    memcpy(p->nodes, original->nodes, MAX_PATH_SIZE*sizeof(int));
+    p->size = original->size;
+    p->cost = original->cost;
+    return p;
+}
+
+/**
+ * Adiciona o nó n ao fim da lista de nós do path p
+ * O tamanho da lista (p->size) é atualizado, mas
+ * o custo (p->cost) não.
+ * 
+ * @param p o caminho
+ * @param n o vértice a ser concatenado à lista
+ * 
+ * @returns void
+ * 
+*/
+void concatenate_to_path(path* p, int n){
+    p->nodes[p->size++] = n;
+}
+
+/**
+ * Obtém o custo de um path com base na matriz
+ * de adjacências passada. Se o custo já tiver
+ * sido calculado, ele apenas é retornado.
+ * 
+ * @param p o path
+ * @param adj a matriz de adjacências
+ * 
+ * @returns o custo do caminho p, de acordo com adj
+*/
+int get_path_cost(path* p, int** adj){
+    if(p->cost == COST_NOT_COMPUTED){
+        for(int i = 0; i < (p->size-1); i++){
+            p->cost += adj[p->nodes[i]][p->nodes[i+1]];
+        }
+    }
+
+    return p->cost;
+}
+
+/*
+*********** Utilidades para path lists **********
+*/
+
+/*
+TODO:
+- criar path list
+- liberar path list
+- criar path list com um nó só
+- obter o custo de todos os elementos na path list
+- fundir duas path lists
+*/
+
+
+/*
 ********* Funções do problema principal *********
 */
 
@@ -103,6 +212,39 @@ int** get_cost_matrix(int n){
     return matrix;
 }
 
+/**
+ * Resolve o problema para um dado n, uma lista de adjacências e o caminho inicial
+ * 
+ * @param n o número de nós no grafo
+ * @param adj a lista de adjacências do grafo, com os pesos
+ * @param initial_path o caminho inicial
+ * 
+ * @returns uma lista de caminhos com o menor custo
+ * 
+*/
+path_list solve_problem(int n, int** adj, path* initial_path){
+    if(initial_path->size == n){ //Caso base
+        concatenate_to_path(initial_path, STARTING_NODE);
+        //TODO: retorna lista com só o caminho inicial
+    }
+    else{
+        //Lista de dados
+        int visited[MAX_GRAPH_SIZE];
+        for(int i = 0; i < MAX_GRAPH_SIZE; i++){
+            visited[i] = 0;
+        }
+
+        for(int i = 0; i<initial_path->size; i++){
+            visited[initial_path->nodes[i]] = 1;
+        }
+
+        //TODO: adicionar todos não visitados
+        //e então obter a lista de todos com o
+        //menor custo, liberar as path lists
+        //de caminhos mais caros que esse menor custo,
+        //mergear as restantes e retornar
+    }
+}
 
 int main(int argc, char** argv){
     if(argc < 2){
@@ -111,8 +253,19 @@ int main(int argc, char** argv){
     }
 
     int n = atoi(argv[1]);
+
+    if(n + 1 > MAX_GRAPH_SIZE){
+        printf("O N passado é maior que o limite do programa. Isso pode ser resolvido se modificando o macro MAX_GRAPH_SIZE, na linha 16, e recompilando o programa. \n");
+        return 1;
+    }
+
     int** costs = get_cost_matrix(n);
-    print_matrix(costs,n,n);
+    path* initial_path = new_path;
+    concatenate_to_path(initial_path, STARTING_NODE);
+
+    solve_problem(n,costs, initial_path);
+
+    delete_path(initial_path);
     delete_matrix(costs,n);
 
     return 0;
