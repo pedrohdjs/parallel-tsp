52c76025042c87234dfc31ebe0096cebf6821866
Pedro Souza <pedrohdjs@outlook.com>
2022-11-30 11:46:45 -0300

    Finished sequential implementation

--- pcv-seq.c
+++ pcv-seq.c
@@ -18,6 +18,7 @@
 #define STARTING_NODE 0
 #define COST_NOT_COMPUTED 0
 #define PATH_LIST_SIZE 32
+#define PATH_LIST_EMPTY -1
 
 typedef struct _path { //Um caminho
     int nodes[MAX_PATH_SIZE]; //Os nós no caminho
@@ -27,6 +28,8 @@ typedef struct _path { //Um caminho
 
 typedef struct _path_list{ //Uma lista de caminhos alocados dinamicamente. Os caminhos nessa lista sempre devem ter o mesmo custo.
     path** paths;
+    int size;
+    int _actual_size;
 } path_list;
 
 /*
@@ -75,6 +78,7 @@ void delete_matrix(int** matrix, int n){
         free(matrix[i]);
     }
     free(matrix);
+    matrix = NULL;
 }
 
 /**
@@ -122,6 +126,7 @@ path* new_path(){
 */
 void delete_path(path* p){
     free(p);
+    p = NULL;
 }
 
 /**
@@ -175,19 +180,120 @@ int get_path_cost(path* p, int** adj){
     return p->cost;
 }
 
+/**
+ * Imprime um caminho
+ * 
+ * @param p o caminho
+ * 
+ * @returns void
+*/
+void print_path(path* p){
+    for(int i = 0; i<p->size-1; i++){
+        printf("%d -> ", p->nodes[i]);
+    }
+    printf("%d\n", p->nodes[p->size-1]);
+}
+
 /*
 *********** Utilidades para path lists **********
 */
 
-/*
-TODO:
-- criar path list
-- liberar path list
-- criar path list com um nó só
-- obter o custo de todos os elementos na path list
-- fundir duas path lists
+/**
+ * Aloca uma nova path list dinamicamente
+ * 
+ * @returns uma path list nova e vazia, alocada dinamicamente
 */
+path_list* new_path_list(){
+    path_list* pl = (path_list*)malloc(1*sizeof(path_list));
+    pl->paths = (path**)malloc(PATH_LIST_SIZE*sizeof(path*));
+    pl->size = 0;
+    pl->_actual_size = PATH_LIST_SIZE;
+}
 
+
+/**
+ * Libera o espaço de uma path list, sem liberar
+ * os paths dentro dela
+ * 
+ * @param pl a path list a ser liberada
+ * 
+ * @returns void
+*/
+void delete_path_list(path_list* pl){
+    free(pl->paths);
+    free(pl);
+    pl = NULL;
+}
+
+
+/**
+ * Libera os paths dentro de uma path list,
+ * sem liberar a path list
+ * 
+ * @param pl a path list que terá os paths associados liberados
+ * 
+ * @returns void
+*/
+void delete_path_list_paths(path_list* pl){
+    for(int i = 0; i < pl->size; i++){
+        delete_path(pl->paths[i]);
+    }
+    pl->size = 0;
+}
+
+/**
+ * Concatena um path a uma path list, alocando
+ * mais memória para a path list se necessário
+ * 
+ * @param pl a path list a receber o path
+ * @param p o path a ser incluso na path list
+ * 
+ * @returns void
+*/
+void concatenate_to_path_list(path_list* pl, path* p){
+    if(pl->size == pl->_actual_size){ //Aloca espaço adicional se necessário
+        int new_size = 2*pl->_actual_size;
+        pl->paths = realloc(pl->paths, new_size*sizeof(path*));
+        pl->_actual_size = new_size;
+    }
+
+
+    pl->paths[pl->size++] = p;
+}
+
+/**
+ * Retorna o custo dos paths em uma path list ou
+ * PATH_LIST_EMPTY caso a path list esteja vazia.
+ * Vale ressaltar que uma path list deve apenas
+ * armazenar paths com o mesmo exato custo.
+ * 
+ * @param pl a path list
+ * @param adj a matriz de adjacências do grafo
+ * 
+ * @returns o custo do primeiro caminhoa na path list ou PATH_LIST_EMPTY, se a path list estiver vazia
+*/
+int get_path_list_paths_cost(path_list* pl, int** adj){
+    if(pl->size == 0){
+        return PATH_LIST_EMPTY;
+    }
+
+    return get_path_cost(pl->paths[0], adj);
+}
+
+/**
+ * Junta duas path lists em uma
+ * 
+ * @param dest uma das path lists, onde o resultado ficará armazenado
+ * @param other a outra path list, que terá seus paths concatenados à path_list dest e será liberada
+ * 
+ * @returns void
+*/
+void merge_path_lists(path_list* dest, path_list* other){
+    for(int i = 0; i<other->size; i++){
+        concatenate_to_path_list(dest, other->paths[i]);
+    }
+    delete_path_list(other);
+}
 
 /*
 ********* Funções do problema principal *********
@@ -213,22 +319,27 @@ int** get_cost_matrix(int n){
 }
 
 /**
- * Resolve o problema para um dado n, uma lista de adjacências e o caminho inicial
+ * Resolve o problema para um dado n, uma lista de adjacências e o caminho inicial.
+ * Esse algoritmo é uma busca em profundidade.
  * 
  * @param n o número de nós no grafo
  * @param adj a lista de adjacências do grafo, com os pesos
- * @param initial_path o caminho inicial
+ * @param initial_path o caminho inicial. Esse caminho será deletado pela função após utilizado.
  * 
  * @returns uma lista de caminhos com o menor custo
- * 
 */
-path_list solve_problem(int n, int** adj, path* initial_path){
+path_list* solve_problem(int n, int** adj, path* initial_path){
+    //printf("Solve problem chamado para o caminho: ");
+    //print_path(initial_path);
     if(initial_path->size == n){ //Caso base
         concatenate_to_path(initial_path, STARTING_NODE);
-        //TODO: retorna lista com só o caminho inicial
+        path* res = copy_path(initial_path);
+        path_list* pl = new_path_list();
+        concatenate_to_path_list(pl,res);
+        return pl;
     }
     else{
-        //Lista de dados
+        //Lista de caminhos visitados
         int visited[MAX_GRAPH_SIZE];
         for(int i = 0; i < MAX_GRAPH_SIZE; i++){
             visited[i] = 0;
@@ -238,11 +349,67 @@ path_list solve_problem(int n, int** adj, path* initial_path){
             visited[initial_path->nodes[i]] = 1;
         }
 
-        //TODO: adicionar todos não visitados
-        //e então obter a lista de todos com o
-        //menor custo, liberar as path lists
-        //de caminhos mais caros que esse menor custo,
-        //mergear as restantes e retornar
+        path_list** pll = (path_list**)malloc(n*sizeof(path_list*)); //Lista de todos os path lists gerados
+        int min_cost = __INT_MAX__; //Custo mínimo dos caminhos
+
+        for(int i = 0; i<n; i++){
+            pll[i] = new_path_list();
+
+            if(!visited[i]){ //Somente processar path lists geradas a partir de um nó não visitado
+                path* p = copy_path(initial_path);
+                concatenate_to_path(p,i);
+
+                pll[i] = solve_problem(n, adj, p);
+            }
+
+            // Obtém o custo mínimo dos caminhos possíveis
+            int cost = get_path_list_paths_cost(pll[i], adj);
+            if(cost != PATH_LIST_EMPTY && cost < min_cost){
+                min_cost = cost;
+            }
+        }
+
+        // O caminho inicial não tem mais uso
+        delete_path(initial_path);
+
+        path_list* res = new_path_list(); //A path list que armazena a resposta
+
+        for(int i = 0; i<n; i++){
+            if(get_path_list_paths_cost(pll[i], adj) == min_cost){
+                merge_path_lists(res,pll[i]);
+                
+            }
+            else{
+                delete_path_list_paths(pll[i]); //Esses caminhos não serão mais utilizados
+                delete_path_list(pll[i]);
+            }
+        }
+
+        free(pll);
+        return res;
+    }
+}
+
+/**
+ * Imprime a resposta
+ * 
+ * @param pl a path list da solução
+ * @param adj a matriz de adjacências do grafo
+ * @param n a ordem do grafo do problema
+ * 
+ * @returns void
+*/
+void print_answer(path_list* pl, int** adj, int n){
+    int cost = get_path_list_paths_cost(pl, adj);
+
+    printf("Matriz de adjacências: \n");
+    print_matrix(adj, n, n);
+    
+    printf("\nCaminhos encontrados, com custo %d: \n", cost);
+    for(int i = 0; i<pl->size; i++){
+        path* p = pl->paths[i];
+
+        print_path(p);
     }
 }
 
@@ -260,12 +427,14 @@ int main(int argc, char** argv){
     }
 
     int** costs = get_cost_matrix(n);
-    path* initial_path = new_path;
+    path* initial_path = new_path();
     concatenate_to_path(initial_path, STARTING_NODE);
 
-    solve_problem(n,costs, initial_path);
+    path_list* res = solve_problem(n,costs, initial_path);
+    print_answer(res, costs, n);
 
-    delete_path(initial_path);
+    delete_path_list_paths(res);
+    delete_path_list(res);
     delete_matrix(costs,n);
 
     return 0;
